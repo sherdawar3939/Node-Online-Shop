298485fa4218b853765a4b448e4478f09c3f5c3c
sher dawar <sherdawar000@gmail.com>
2020-07-24 13:51:36 +0500

    repo created

---
+++ util/database.js
@@ -0,0 +1,32 @@
+// const mongodb = require('mongodb');
+// const MongoClient =mongodb.MongoClient;
+
+// let _db ;
+
+// const mongoConnect = callback=>{
+// MongoClient.connect('mongodb+srv://Sher:9WHqHga8LtCfQV04@cluster0-cvpdb.mongodb.net/businessApp2?retryWrites=true&w=majority',
+// { useUnifiedTopology: true })
+// .then(client=>{
+//     console.log('connected');
+//     _db =client.db()
+//     callback()
+// })
+// .catch(err=>{
+//     console.log(err);
+//     throw err
+// })
+    
+    
+// }
+
+// const getDb = ()=>{
+//     if(_db){
+//         return _db
+//     }
+//     throw ('No data base found')
+// }
+//   exports.mongoConnect = mongoConnect;
+//   exports.getDb = getDb;
+
+
+
